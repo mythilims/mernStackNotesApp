e3c854892e881e136e126f2342b6754fac6dfd6e
mythili <mythilijobofficial@gmail.com>
2025-07-05 18:59:03 +0530

    outDir: 'dist'

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react(),tailwindcss()],
     base:'/',
   build: {
+       outDir: 'dist',
        chunkSizeWarningLimit: 1000,
         rollupOptions: {
             output:{
